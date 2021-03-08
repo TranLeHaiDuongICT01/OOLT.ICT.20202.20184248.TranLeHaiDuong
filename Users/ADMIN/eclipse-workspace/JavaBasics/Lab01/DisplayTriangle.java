065e8c8281285414b4161f79e2908bfcf14f9d62
TranLeHaiDuongICT01 <Duong.tlh.909@aptechlearning.edu.vn>
2021-03-08 17:18:42 +0700

    My homework

---
+++ Users/ADMIN/eclipse-workspace/JavaBasics/Lab01/DisplayTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DisplayTriangle{
+	public static void main(String[] args) {
+		Scanner key=new Scanner(System.in);
+		System.out.println("Input n: ");
+		int n=key.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<n-i+1;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=2*(i-1)+1;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		key.close();
+	}
+}
