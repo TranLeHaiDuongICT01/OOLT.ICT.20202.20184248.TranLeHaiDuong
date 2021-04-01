7f538ec3f40bba3e78b4a78fceb3c8a32251981f
TranLeHaiDuongICT01 <80091158+TranLeHaiDuongICT01@users.noreply.github.com>
2021-04-01 10:02:10 +0700

    Delete Users/ADMIN directory

--- Users/ADMIN/web_giftshop/admincp/modules/menu.php
+++
@@ -1,6 +0,0 @@
-<ul class="admincp_list">
-    <li><a href="index.php?action=quanlydanhmuc&query=add">Quản lý danh mục sản phẩm</a></li>
-    <li><a href="index.php?action=productmanage&query=add">Quản lý sản phảm</a></li>
-    <li><a href="index.php?action=quanlybaiviet&query=add">Quản lý bài viết</a></li>
-    <li><a href="index.php?action=quanlydanhmucbaiviet&query=add">Quản lý danh mục bài viết</a></li>
-</ul>
